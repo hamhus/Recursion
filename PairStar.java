ba8b7142e742b7c81e453493bf5db8076dc67513
Hameed Hussain <hamhus@gmail.com>
2021-07-06 16:01:33 -0700

    Update PairStar.java

--- PairStar.java
+++ PairStar.java
@@ -1,3 +1,11 @@
+//https://codingbat.com/prob/p158175
+/*Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+
+pairStar("hello") → "hel*lo"
+pairStar("xxyy") → "x*xy*y"
+pairStar("aaaa") → "a*a*a*a"*/
+
 public String pairStar(String str) {
   if(str.length()==0) return "";
   if(str.length()==1) return str;
